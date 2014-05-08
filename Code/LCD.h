f37d5ef5a7fe249289718799c0eda06c306ec7b0
RationalAsh <ashwinnarayan1994@gmail.com>
2014-05-08 19:59:56 +0530

    Added a lot of new LCD functions

--- Code/LCD.h
+++ Code/LCD.h
@@ -6,12 +6,17 @@
 #include <util/delay.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
  
 #define dataPORT PORTD    	// In my case PORTB is the PORT from which I send data to my LCD
 #define conPORT PORTC		// In my case PORTC is the PORT from which I set the RS , R/W and En
 #define EN PORTC4		// Enable signal
 #define RW PORTC3		// Read/Write signal
 #define RS PORTC2		// Register Select signal
+#define line0 0x80
+#define line1 0xC0
+#define line2 0x94
+#define line3 0xD4
 
 void writeCom(unsigned char com)
 {
@@ -37,9 +42,50 @@ void writeChar(unsigned char ch)
 void initLCD()
 {
     writeCom(0x3C);
-    writeCom(0x0F);
+    writeCom(0x0C);
     writeCom(0x06);
     writeCom(0x01);
     writeCom(0x80);
 }
+
+void writeString(unsigned char* msg)
+{
+    int len = strlen(msg);
+    int i = 0;
+    for(i=0; i<len; i++)
+    {
+	writeChar(msg[i]);
+    }
+}
+
+void gotoXY(int X, int Y)
+{
+    int com = 0x00;
+    switch(Y)
+    {
+    case 0:
+	com = line0 + X;
+	break;
+    case 1:
+	com = line1 + X;
+	break;
+    case 2:
+	com = line2 + X;
+	break;
+    case 3:
+	com = line3 + X;
+	break;
+    }
+
+    writeCom(com);
+}
     
+void writeAt(int X, int Y, unsigned char* msg)
+{
+    gotoXY(X,Y);
+    int len = strlen(msg);
+    if(len < 20-X)
+    {
+	writeString(msg);
+    }
+}
--- Code/main.c
+++ Code/main.c
@@ -3,6 +3,7 @@
 #include <util/delay.h>
 #include "LCD.h"
 #include "I2C.h"
+#include <string.h>
 #define LED PB0
 
 int main()
@@ -10,14 +11,13 @@ int main()
     DDRB |= 0xFF; //Make all the pins output
     DDRC |= (1<<PC2)|(1<<PC3)|(1<<PC4);
     DDRD |= 0xFF;
-    unsigned char* msg = "Hello World!";
-    
+    unsigned char* msg = "Hello Word!";
+  
     initLCD();
-    int i = 0;
-    for(i=0;i<14;i++)
-    {
-	writeChar(msg[i]);
-    }
+    writeString(msg);
+    gotoXY(10,2);
+    //writeCom(0x94 + 0x0A);
+    writeAt(10,2,"ABBA");
            
     while(1)
     {
